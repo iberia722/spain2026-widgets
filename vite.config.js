ee86abca8cf5c310a8d72af227d73b8321221395
iberia722 <cantjr@gmail.com>
2026-03-06 18:13:17 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'briefing.html', dest: '.' },
+        { src: 'dashboard.html', dest: '.' },
+        { src: 'barcelona-map.html', dest: '.' },
+        { src: 'barcelona-weather.html', dest: '.' },
+        { src: 'madrid-weather.html', dest: '.' },
+        { src: 'zaragoza-map.html', dest: '.' },
+        { src: 'zaragoza-weather.html', dest: '.' },
+      ]
+    })
+  ],
 })
